4cf9c744af62163868065ea3d99ac98aa2c7ca2e
saurav-kr <114326000+saurav-kumar-mce@users.noreply.github.com>
2022-12-16 09:35:20 +0530

    commit

--- providers.tf
+++ providers.tf
@@ -1,9 +1,3 @@
 provider "aws" {
   region     = "ap-northeast-1"
-  access_key = "AKIA4WHVUN47QYKPX25S"
-  secret_key = "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"
-
-  #if you are running from AWS ec2 linux instance please use bellow credentials section
-  #shared_credentials_file = "$HOME/.aws/credentials"
-  #profile = "default"
 }
